e6c8a34f6442543db4a922a99cbb149d4efbd9f1
Justin Raitz <jmr18c@acu.edu>
2020-10-13 08:44:42 -0500

    Initial Commit.

---
+++ My Swifty Dice App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Swifty Dice App
+//
+//  Created by Justin Raitz on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
