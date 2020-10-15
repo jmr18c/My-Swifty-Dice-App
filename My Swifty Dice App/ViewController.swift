2b7a08a4107566756d71ef5ec441cefc20868b05
jmr18c <jmr18c@acu.edu>
2020-10-15 09:37:55 -0500

    Everything works and looks beautiful! Just need to add sounds now.

--- My Swifty Dice App/ViewController.swift
+++ My Swifty Dice App/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonGotPressed(){
+        rollDice()
+    }
+    
+    func rollDice(){
+        //do cool stuff here
+        print("WE ROLLIN' BRO!")
+        
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+        
+        if (imageName == "d1"){
+            criticalLabel.text = "SUX 2 SUK!"
+        } else if (imageName == "d20"){
+            criticalLabel.text = "YOU ARE WINNER!"
+        } else {
+            criticalLabel.text = ""
+        }
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+    }
 
 }
 
